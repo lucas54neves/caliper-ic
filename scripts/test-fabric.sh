05342b3578e54233b5ef90d8b094f03fc686cb2a
Lucas Neves <lucas54neves@gmail.com>
2020-07-31 08:32:16 -0300

    Add new report round

--- scripts/test-fabric.sh
+++ scripts/test-fabric.sh
@@ -3,6 +3,7 @@
 cd ../caliper
 
 npm run fabric-v1.1-deps
+npm install --no-save grpc@1.10.1 fabric-ca-client@1.1.0 fabric-client@1.1.0
 
 sudo npm run bench -- -c benchmark/simple/config-benchmark.yaml -n network/fabric-v1.1/dev/fabric-node.json
 
