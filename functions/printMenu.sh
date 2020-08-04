422a9d3bcac025164a97f0d80d61d974831dbc25
Lucas Neves <lucas54neves@gmail.com>
2020-08-04 13:31:33 -0300

    Add function that prints the menu

---
+++ functions/printMenu.sh
@@ -0,0 +1,17 @@
+#!bin/bash
+
+# Print menu
+function printMenu() {
+    echo
+    echo '########################################'
+    echo '################# Menu #################'
+    echo '########################################'
+    echo '[1] - Install prerequisites for Linux'
+    echo '[2] - Install Caliper dependencies'
+    echo '[3] - Test all blockchains'
+    echo '[4] - Test Burrow'
+    echo '[5] - Test Fabric'
+    echo '[0] - Close program'
+    echo '########################################'
+    echo
+}
